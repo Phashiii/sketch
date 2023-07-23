4c64a18d231814ac0869bad319f27f2c7092be21
Thato <thatojakemoema@gmail.com>
2023-07-23 08:53:59 +0200

    Enabled the mouse to draw on grid squares

--- js_script.js
+++ js_script.js
@@ -8,20 +8,28 @@ for(let i = 0; i < 16; i++)
     column = document.createElement('div');
     column.style.height = "16px";
     column.style.width = "16px";
-    //column.style.border = "thin solid #0000FF";
-    
-    for(let j = 1; j < 16; j++)
+    column.className ='draw';
+    for(let j = 0; j < 16; j++)
     {
         row = document.createElement('div');
         row.style.height = "16px";
         row.style.width = "16px";
         row.style.border = "thin solid #0000FF";
+        row.className ='draw';
         column.appendChild(row);
     }
     container.appendChild(column);
 }
 container.style.display = "flex";
+container.style.justifyContent= "center";
 
+{
+container.addEventListener('mouseover', (event) =>
+{
+    if(event.target.className === "draw")
+    event.target.style.backgroundColor = "pink";
+})
+}
 
 
 
